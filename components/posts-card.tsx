800849e9eb0623ebf874594d3ddb036ba9b73401
ibrahim <karroumiibrahim33@gmail.com>
2024-02-22 14:55:56 +0100

    add some styling to cards

--- components/posts-card.tsx
+++ components/posts-card.tsx
@@ -6,12 +6,12 @@ interface PostsCardI {
 const PostCard = ({ post, onClick = () => {} }: PostsCardI) => {
   return (
     <div
-      className="p-6 border border-gray-200 rounded-lg cursor-pointer hover:shadow" //border border-gray-200 p-6 rounded-lg
+      className="p-6 border border-gray-200 rounded-lg cursor-pointer hover:shadow hover:bg-gray-600 hover:text-white" //border border-gray-200 p-6 rounded-lg
       onClick={() => {
         onClick();
       }}
     >
-      <h2 className="mb-2 text-lg font-medium text-gray-900 sm:text-xl title-font">{post.title}</h2>
+      <h2 className="mb-2 text-lg font-medium sm:text-xl title-font">{post.title}</h2>
       <p className="mb-4 text-base leading-relaxed">{post.body}</p>
     </div>
   );
