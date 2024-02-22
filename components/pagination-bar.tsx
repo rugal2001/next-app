22b04083568eb69c0aa89ddfea8da450712c7e6b
ibrahim <karroumiibrahim33@gmail.com>
2024-02-22 14:55:34 +0100

    formating

--- components/pagination-bar.tsx
+++ components/pagination-bar.tsx
@@ -1,28 +1,46 @@
 import { useState } from "react";
 
-export default function PaginationL({ Map, current, pageSize ,handlePreviousPage ,handleNextPage  }) {
+export default function PaginationL({
+  Map,
+  current,
+  pageSize,
+  handlePreviousPage,
+  handleNextPage,
+}) {
   return (
     <>
-      <Pagination table={Map} current={current} pageSize={pageSize}   handleNextPage={handleNextPage} handlePreviousPage={handlePreviousPage}/>
+      <Pagination
+        table={Map}
+        current={current}
+        pageSize={pageSize}
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+      />
     </>
   );
 }
 
-function Pagination({ table, current, pageSize , handleNextPage  , handlePreviousPage}) {
+function Pagination({
+  table,
+  current,
+  pageSize,
+  handleNextPage,
+  handlePreviousPage,
+}) {
   const [currentPage, setCurrentPage] = useState(current);
   const totalCount = table.length;
   const totalPages = totalCount;
-
-
 
   return (
     <div className="flex items-center justify-center gap-5">
       <div className="btn" onClick={handlePreviousPage}>
         Previous
       </div>
-      {<div>{current}</div>}
-      {<div>/</div>}
-      {<div>{totalPages}</div>}
+      <div className="flex justify-center gap-3 text-gray-700">
+        {<div>{current}</div>}
+        {<div>/</div>}
+        {<div>{totalPages}</div>}
+      </div>
 
       <div className="btn" onClick={handleNextPage}>
         Next
