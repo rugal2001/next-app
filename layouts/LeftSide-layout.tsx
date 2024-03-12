19aa4c746bae66aa4cb90f0cb54d7169e8724591
ibrahim <karroumiibrahim33@gmail.com>
2024-03-12 14:15:09 +0000

    some deleted files

--- layouts/LeftSide-layout.tsx
+++
@@ -1,9 +0,0 @@
-const sideBar = ()=>{
-    return (
-        <div className="">
-            <div className="fixed flex items-center h-10 pl-2 m-1 my-20 text-xl rounded-md cursor-pointer hover:bg-gray-300 w-72">Left Side Bar</div>
-            
-        </div>
-    );
-}
-export default sideBar;
--- layouts/Main.tsx
+++
@@ -1,7 +0,0 @@
-const MainL = ({children})=>{
-    return (
-        <div className="flex justify-center w-full">{children}</div>
-    );
-}
-
-export default MainL
