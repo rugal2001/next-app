de71c538021250ffbb4abf02feeb0e98cf1dde6c
ibrahim <karroumiibrahim33@gmail.com>
2024-03-12 13:05:19 +0000

    support the fetcher with httpClientReq

--- lib/fetcher.tsx
+++ lib/fetcher.tsx
@@ -1,10 +1,20 @@
 import axios from "axios";
-import httpClientReq from "./httpClientReq";
+import httpClientReq from "./http-client-req";
 
-const fetcher = (url: string) =>
-  httpClientReq(url).then((response) => response.data);
+const fetcher = async (url: string) => {
+  try {
+    const response = await httpClientReq(url);
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch data");
+    }
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching data: ${error.message}`);
+  }
+};
 
 export default fetcher;
+
 
 // //const fetcher = async (url) => {
 //   const response = await axios.get(url);
