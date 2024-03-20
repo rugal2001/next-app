ded59f0cc0923ef3aca7dc8ebac472f3c070a17a
ibrahim <karroumiibrahim33@gmail.com>
2024-03-20 11:27:45 +0000

    Support the 3d card

--- components/posts-card.tsx
+++ components/posts-card.tsx
@@ -1,25 +1,12 @@
-import { HiMiniEllipsisVertical } from "react-icons/hi2";
-import { AppShell, Burger, Avatar, Portal, Modal, Alert } from "@mantine/core";
-import { useEffect, useState } from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { CardBody, CardContainer, CardItem } from "./3d-card";
+import { Avatar, Modal } from "@mantine/core";
+import { IconInfoCircle } from "@tabler/icons-react";
 import useSWR, { mutate } from "swr";
 import fetcher from "../lib/fetcher";
-import { IoIosClose } from "react-icons/io";
-//////////////////////////////////////////////////////////
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../components/action-popup";
 import axios from "axios";
-import { IconInfoCircle } from "@tabler/icons-react";
-
-//////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////
-
+import { IoIosClose } from "react-icons/io";
 interface PostsCardI {
   post: {
     _id: string;
@@ -31,7 +18,6 @@ interface PostsCardI {
   onClick?: () => any;
   onUpdate: any;
 }
-
 const PostNewCard = ({ onUpdate, post, onClick = () => {} }: PostsCardI) => {
   const { data, isLoading, error } = useSWR("/me", fetcher);
   const icon = <IconInfoCircle />;
@@ -40,22 +26,20 @@ const PostNewCard = ({ onUpdate, post, onClick = () => {} }: PostsCardI) => {
   const [opened, setOpened] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
 
-  const contenue = post.contenue;
-
-  const words = contenue.split(" ");
+  const contenue = post?.contenue;
+  const words = contenue?.split(" ");
 
   const maxWords = 20;
 
-  const shortenedContent = words.slice(0, maxWords).join(" ");
+  const shortenedContent = words?.slice(0, maxWords).join(" ");
 
   const displayContent =
-    words.length > maxWords ? shortenedContent + "..." : shortenedContent;
+    words?.length > maxWords ? shortenedContent + "..." : shortenedContent;
 
   if (!post || !post.contenue) {
     return null;
   }
 
-  
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   const handleDeletePost = async () => {
     try {
@@ -102,7 +86,7 @@ const PostNewCard = ({ onUpdate, post, onClick = () => {} }: PostsCardI) => {
       console.error("handle Update post ", error);
     }
     onUpdate();
-    console.log('onUpdate();',onUpdate())
+    console.log("onUpdate();", onUpdate());
   };
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -208,82 +192,58 @@ const PostNewCard = ({ onUpdate, post, onClick = () => {} }: PostsCardI) => {
           </div>
         </Modal>
       )}
-      <div className="p-1 bg-white border border-gray-200 rounded-lg hover:shadow w-[65%]">
-        <div className="w-full bg-white rounded-t-lg ">
-          <div className="flex justify-between ">
-            <div className="flex items-center gap-3 p-2 text-xl font-semibold">
-              <Avatar
-                src={post?.user?.image}
-                alt="it's me"
-                className="cursor-pointer"
-              />
-              {post?.user?.firstName} {post?.user?.lastName}
+      <CardContainer className="inter-var">
+        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <CardItem
+            translateZ="50"
+            className="w-full text-xl font-bold text-neutral-600 dark:text-white"
+          >
+            <div className="flex justify-between w-full ">
+              <div className="flex items-center gap-3 p-2 text-xl font-semibold">
+                <Avatar
+                  src={post?.user?.image}
+                  alt="it's me"
+                  color="cyan"
+                  className="cursor-pointer "
+                >
+                  {post.user?.firstName.toUpperCase().charAt(0)}
+                  {post.user?.lastName.toUpperCase().charAt(0)}
+                </Avatar>
+                {post?.user?.firstName} {post?.user?.lastName}
+              </div>
+              
             </div>
-            <div className="flex items-center pr-2 " onClick={() => {}}>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <HiMiniEllipsisVertical className="text-3xl rounded-full cursor-pointer hover:bg-gray-100" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-48 h-auto bg-white">
-                  {/* <DropdownMenuLabel>Outils</DropdownMenuLabel> */}
-                  {/* <DropdownMenuSeparator /> */}
-                  {post?.user._id === data?._id || data?.role==='admin' ? (
-                    <DropdownMenuItem
-                      className="font-bold cursor-pointer hover:bg-gray-100"
-                      onClick={() => setOpened(true)}
-                    >
-                      <div>Edit</div>
-                    </DropdownMenuItem>
-                  ) : null}
-                  {post?.user._id === data?._id || data?.role==='admin' ? (
-                    <DropdownMenuItem
-                      className="font-bold cursor-pointer hover:bg-gray-100"
-                      onClick={() => {
-                        setShowConfirmation(true);
-                      }}
-                    >
-                      Delete
-                    </DropdownMenuItem>
-                  ) : null}
-                  <DropdownMenuItem className="font-bold cursor-pointer hover:bg-gray-100">
-                    Info
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </div>
-        </div>
-        <div className="flex rounded-md">
-          <div className="grid ">
-            <div className=" p-3 bg-white rounded-r-lg">
-              <p className="text-base leading-relaxed ">
-                {displayContent}
-                {words.length > maxWords && (
-                  <button
-                    onClick={() => onClick()}
-                    className="font-semibold text-blue-600"
-                  >
-                    VoirPlus
-                  </button>
-                )}
-              </p>
-            </div>
-
-            <div className="w-full max-x-lg" >
-              <img
-                src={post.image}
-                className=" rounded-lg cursor-pointer"
-                // style={{ height: "15cm" }}
-                onClick={() => {
-                  onClick();
-                }}
-              ></img>
-            </div>
-          </div>
-        </div>
-      </div>
+          </CardItem>
+          <CardItem
+            as="p"
+            translateZ="60"
+            className="max-w-sm mt-2 text-sm text-neutral-500 dark:text-neutral-300"
+          >
+            {displayContent}
+            {words.length > maxWords && (
+              <button
+                onClick={() => onClick()}
+                className="font-semibold text-blue-600"
+              >
+                VoirPlus
+              </button>
+            )}
+          </CardItem>
+          <CardItem translateZ="100" className="w-full mt-4">
+            <img
+              src={post.image}
+              height="1000"
+              width="1000"
+              className="object-cover w-full cursor-pointer h-60 rounded-xl group-hover/card:shadow-xl"
+              alt="thumbnail"
+              onClick={() => {
+                onClick();
+              }}
+            />
+          </CardItem>
+        </CardBody>
+      </CardContainer>
     </>
   );
 };
-
 export default PostNewCard;
