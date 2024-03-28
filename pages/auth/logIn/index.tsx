fe985381eb353403346d52cb8ffcb62ae6c9fcc7
ibrahim <karroumiibrahim33@gmail.com>
2024-03-28 16:28:10 +0000

    .

--- pages/auth/logIn/index.tsx
+++ pages/auth/logIn/index.tsx
@@ -89,6 +89,9 @@ function LogIn() {
               <div className="flex items-center gap-3">
                 <button
                   type="submit"
+                  onClick={() => {
+                    router.push("/");
+                  }}
                   className="px-8 py-2 text-sm text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600"
                 >
                   Log In
