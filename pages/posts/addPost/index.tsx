5d28db16fadfd49bdda9b1b7b711acbe1b755c55
ibrahim <karroumiibrahim33@gmail.com>
2024-03-28 10:45:28 +0000

    Support the upload of the image image in the selection of the image for better performance

--- pages/posts/addPost/index.tsx
+++ pages/posts/addPost/index.tsx
@@ -8,86 +8,74 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import fetcher from "../../../lib/fetcher";
 import AuthLayout from "../../../layouts/auth-layout";
+import httpClientReq from "@/lib/http-client-req";
 
 function Home() {
   const { data, isLoading, error } = useSWR("/me", fetcher);
   const router = useRouter();
-  console.log("this is data in add Post => ", data);
   const [name, setName] = useState(data?.firstName + " " + data?.lastName);
   const [contenue, setContenue] = useState("");
-  const [image, setImage] = useState(
-    "https://www.shutterstock.com/image-vector/no-image-available-icon-template-600nw-1036735678.jpg"
-  );
-  const [image2, setImage2] = useState(
+  const [image, setImage] = useState(null);
+  const [previewImage, setPreviewImage] = useState(
     "https://www.shutterstock.com/image-vector/no-image-available-icon-template-600nw-1036735678.jpg"
   );
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function () {
-      if (typeof reader.result === "string") {
-        setImage(e.target.files[0]);
-        setImage2(reader.result);
-        const imageOnBoard = reader.result;
-      } else {
-        console.error("Failed to read image as string");
-      }
-    };
-
     if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPreviewImage(reader.result as string);
+        setImage(file);
+      };
       reader.readAsDataURL(file);
     }
-    return reader.result;
   };
 
-  const handleSubmit = () => {
-    const formData = new FormData();
-    formData.append("image", image);
-    axios
-      .post("http://localhost:4000/upload-img", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          authorization: `Bearer ${localStorage.getItem("access_token")}`,
-        },
-      })
-      .then((response) => {
-        fetch("http://localhost:4000/posts", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            authorization: `Bearer ${localStorage.getItem("access_token")}`,
-          },
-          body: JSON.stringify({
-            name: name,
-            contenue: contenue,
-            image: response.data,
-            user: data._id,
-          }),
-        })
-          .then((response) => response.json())
-          .then((data) => console.log("data ====> ", data))
-          .then(() => router.push("/posts"))
-          .catch((error) => console.error("Error:", error));
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+  useEffect(() => {
+    if (image instanceof File) {
+      uploadImage();
+    }
+  }, [image]);
+
+  const uploadImage = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("image", image);
+      const response = await httpClientReq.post("/upload-img", formData);
+      setImage(response.data.filePath);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const insertedData = {
+        name: " ",
+        image,
+        contenue,
+        user: data._id,
+      };
+      const response = await httpClientReq.post("/posts", insertedData);
+      router.push("/posts");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <div className="flex justify-center w-[80%] bg-gray-200 h-full ">
+    <div className="flex justify-center w-[100%] bg-gray-300 h-screen">
       <div className="h-full w-[70%]">
         <div className="grid items-center justify-center gap-5 ">
           <div className="flex justify-center mt-4 text-5xl font-semibold">
             Add Post
           </div>
           <div className="w-[100%] flex justify-center  max-x-lg">
-            <div className="w-[100%] p-2 mb-3 bg-white rounded-md max-x-lg ">
+            <div className="p-2 mb-3 bg-white rounded-md w-96 max-x-lg ">
               <div className="w-full max-x-lg" style={{ background: "cover" }}>
                 <img
-                  src={image2}
+                  src={previewImage}
                   alt="Uploaded"
                   className="w-full rounded-md"
                 ></img>
@@ -98,11 +86,12 @@ function Home() {
                   className="flex items-center gap-3 cursor-pointer w-[50%]"
                   onClick={() => document.getElementById("fileInput").click()}
                 >
-                  <div className="w-10 ">
+                  <div className="w-10 my-2">
                     <CiImageOn className="w-full h-full text-lime-600" />
                   </div>
-                  <div className="w-full font-bold ">Upload Image</div>
+                  <div className="w-full font-semibold text-black">Upload Image</div>
                   <input
+                 
                     id="fileInput"
                     type="file"
                     accept="image/*"
@@ -113,7 +102,7 @@ function Home() {
 
                 <div className="flex gap-5">
                   <textarea
-                    className="w-[90%] h-auto px-3 rounded-b-lg"
+                    className="w-[90%] h-12 px-3 rounded-lg"
                     placeholder="Dites quelque chose à propos de cette photo ..."
                     style={{ outline: "none", overflowY: "hidden" }}
                     rows={1}
@@ -121,7 +110,7 @@ function Home() {
                     onChange={(e) => setContenue(e.target.value)}
                   ></textarea>
 
-                  <div className="w-[5%]">
+                  <div className="w-8">
                     <IoSend
                       name="submit"
                       className="w-full h-full text-blue-600 cursor-pointer"
@@ -141,10 +130,9 @@ function Home() {
 Home.GetLayout = function GetLayout(Home) {
   return (
     <AuthLayout>
-      <Header hideAddButton={true} />
-      
-        <Main>{Home}</Main>
-      
+      {/* <Header hideAddButton={true} /> */}
+
+      <Main>{Home}</Main>
     </AuthLayout>
   );
 };
