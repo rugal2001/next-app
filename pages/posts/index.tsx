9220f4cce8eff49d3fea55d1f8d46b637aca8baf
ibrahim <karroumiibrahim33@gmail.com>
2024-04-04 12:10:16 +0000

    Apply the infinite scrolling to the posts page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,73 +4,81 @@ import fetcher from "../../lib/fetcher";
 import PostCard from "../../components/posts-card";
 
 import { useEffect, useState } from "react";
-import PaginationL from "../../components/pagination-bar";
 
-import Header from "../../layouts/main-layout/header";
 import Main from "../../layouts/main-layout";
 import AuthLayout from "../../layouts/auth-layout";
-import { Loader } from "@mantine/core";
+import { Skeleton } from "@mantine/core";
+import { useInView } from "react-intersection-observer";
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 4;
 
 function Home() {
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [isEndOfList, setIsEndOfList] = useState<boolean>(false);
+  const [posts, setPosts] = useState({ data: [] });
 
-  const {
-    data: posts,
-    error,
-    isLoading,
-    mutate: mutatePosts,
-  } = useSWR(
+  const { data: newPosts, error } = useSWR(
     `http://localhost:4000/posts?_page=${currentPage}&_limit=${PAGE_SIZE}`,
     fetcher
   );
 
-  if (isLoading) return <div className="grid justify-center mt-80"><Loader color="blue" />;</div>;
-  if (error) return <div>Error Loading Posts</div>;
-  if (!posts) return <div>There are no posts</div>;
+  const { ref: loadMoreRef } = useInView({
+    threshold: 0.1,
+    onChange: (inView) => {
+      if (inView && !isEndOfList) {
+        handleLoadMore();
+      }
+    },
+  });
 
- 
+  useEffect(() => {
+    if (newPosts && !error) {
+      setPosts((prevPosts) => ({
+        data: [...prevPosts.data, ...newPosts.data],
+      }));
+      setIsFetching(false);
+    }
+  }, [newPosts, error]);
 
-  // const totalCount = posts.length;
-  // const totalPages = totalCount;
-  // const handleNextPage = () => {
-  //   if (currentPage < totalPages) {
-  //     setCurrentPage((prev) => prev + 1);
-  //   }
-  // };
+  const handleLoadMore = () => {
+    if (newPosts) {
+      if (newPosts.data.length > 0) {
+        setCurrentPage((prevPage) => prevPage + 1);
+        setIsFetching(true);
+      } else {
+        setIsFetching(false);
+        setIsEndOfList(true);
+      }
+    }
+  };
 
-  // const handlePreviousPage = () => {
-  //   if (currentPage > 1) {
-  //     setCurrentPage((prev) => prev - 1);
-  //   }
-  // };
-
-  const reversedPosts = [...posts.data].reverse();
+  
   return (
     <section className="grid justify-center w-full text-black body-font">
       <div className="p-4 bg-white max-sm:max-w-sm lg:max-w-screen-lg">
-        <div className="flex flex-col items-center gap-12 ">
-          {reversedPosts.map((post) => (
+        <div className="flex flex-col items-center gap-12">
+          {posts.data.map((post) => (
             <PostCard
-              onUpdate={() => mutatePosts()}
-              key={post._id}
               post={post}
-              onClick={() => {
-                router.push(`/posts/${post._id}`);
-              }}
+              onClick={() => router.push(`/posts/${post._id}`)}
+              onUpdate={undefined}
             />
           ))}
+
+          <div ref={loadMoreRef}></div>
+          <Skeleton
+            hidden={isEndOfList}
+            height={420}
+            width={480}
+            radius="md"
+            className="mb-2"
+          />
         </div>
-        {/* <PaginationL
-          Map={posts}
-          current={currentPage}
-          pageSize={PAGE_SIZE}
-          handleNextPage={handleNextPage}
-          handlePreviousPage={handlePreviousPage}
-        ></PaginationL> */}
       </div>
+      {/* {isFetching && <Loader color="blue" />} */}
+      {/* {isEndOfList && <Text className="text-center">End of posts</Text>} */}
     </section>
   );
 }
