74fe390292581bf18d9680e8be16c5e1007b1777
ibrahim <karroumiibrahim33@gmail.com>
2024-04-04 12:06:46 +0000

    .

--- components/pagination-bar.tsx
+++ components/pagination-bar.tsx
@@ -3,17 +3,18 @@ import { useState } from "react";
 export default function PaginationL({
   Map,
   current,
-  pageSize,
+  totalPages,
   handlePreviousPage,
   handleNextPage,
   
 }) {
+  console.log('pagination');
   return (
     <>
       <Pagination
         table={Map}
         current={current}
-        pageSize={pageSize}
+        totalPages={totalPages}
        handleNextPage={handleNextPage}
        handlePreviousPage={handlePreviousPage}
       />
@@ -24,27 +25,28 @@ export default function PaginationL({
 function Pagination({
   table,
   current,
-  pageSize,
+  totalPages,
   handleNextPage,
   handlePreviousPage,
   
 }) {
-  const [currentPage, setCurrentPage] = useState(current);
+  // const [currentPage, setCurrentPage] = useState(current);
   const totalCount = table.length;
-  const totalPages = totalCount;
-  
+  // const totalPages = totalCount;
+  console.log({totalCount})
+  console.log({totalPages})
   return (
-    <div className="flex items-center justify-center gap-5">
-      <div className="btn" onClick={handlePreviousPage}>
+    <div className="flex items-center justify-center gap-5 mt-3">
+      <div className="text-sm text-gray-800 cursor-pointer hover:text-gray-500" onClick={handlePreviousPage}>
         Previous
       </div>
-      <div className="flex justify-center gap-3 text-gray-700">
-        {<div>{current}</div>}
-        {<div>/</div>}
-        {<div>{totalPages}</div>}
+      <div className="flex justify-center gap-3 text-gray-700 ">
+        {<div className="flex items-center justify-center w-8 h-8 text-white bg-blue-600 rounded-md"> {current}</div>}
+        {<div> - </div>}
+        {<div className="flex items-center justify-center w-8 h-8 text-black bg-gray-200 rounded-md">{totalPages}</div>}
       </div>
 
-      <div className="btn" onClick={handleNextPage}>
+      <div className="text-sm text-gray-800 cursor-pointer hover:text-gray-500" onClick={handleNextPage}>
         Next
       </div>
     </div>
