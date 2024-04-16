b46bc1b705b4e95d4e05179dd4f38f92d59ba5f2
ibrahim <karroumiibrahim33@gmail.com>
2024-04-16 17:21:33 +0100

    test

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,31 +1,71 @@
+import fetcher from "../../lib/fetcher";
+
 import Main from "../../layouts/main-layout";
 import AuthLayout from "../../layouts/auth-layout";
-import { Skeleton } from "@mantine/core";
+import { Loader, Skeleton } from "@mantine/core";
+import useSWRInfinite from "swr/infinite";
+import router from "next/router";
+import PostCard from "@/components/posts-card";
 
-import useSWR from "swr";
-import fetcher from "@/lib/fetcher";
-
+const getKey = (pageIndex, previousPageData) => {
+  if (previousPageData && !previousPageData.length) return null;
+  if (pageIndex === 0) {
+    console.log("hello");
+  }
+  console.log({ pageIndex });
+  return `/posts?_page=${pageIndex}&_limit=4`;
+};
 function Home() {
-  return (
-    <>
-       <div className="">
-        {/*<div className="flex items-center gap-3 bg-green-400">
-          <div className="bg-blue-400 ">
-            <Skeleton height={50}  circle mb="xl" />
-          </div>
-          <div className="w-full bg-lime-900">
-            <Skeleton height={8} width={150} radius="xl" />
-          </div>
-        </div>
+  const {
+    data: paginatedPosts,
+    size,
+    isLoading,
+    setSize,
+  } = useSWRInfinite(getKey, fetcher, { parallel: true });
 
-        <Skeleton height={8} radius="xl" />
-        <Skeleton height={8} mt={6} radius="xl" />
-        <Skeleton height={8} my={6}  width="70%" radius="xl" /> */}
-        <Skeleton visible={true} height={230} width={260} className="mb-2" />
-          
-        {/* </Skeleton> */}
+  if (isLoading) {
+    return (
+      <div>
+        <Loader color="red" />
       </div>
-    </>
+    );
+  }
+
+  // let totalPosts = 0;
+  // for (let i = 0; i < paginatedPosts.length; i++) {
+  //   totalPosts += paginatedPosts[i].data.length;
+  // }
+  console.log({ size, paginatedPosts });
+
+  return (
+    <div>
+      <div className="grid gap-12 mt-6">
+        {paginatedPosts.map((posts, index) => {
+          {
+            console.log({ posts });
+          }
+          // `data` is an array of each page's API response.
+          return posts.data.map((post) => (
+            <PostCard
+              key={post._id}
+              post={post}
+              onClick={() => router.push(`/posts/${post._id}`)}
+              onUpdate={undefined}
+            />
+          ));
+        })}
+      </div>
+
+      {/* <button onClick={() => setSize(size + 1)}>Load More</button> */}
+      <button
+        className="grid justify-center px-3 py-2 text-sm font-semibold text-white bg-blue-600 rounded-xl"
+        onClick={() => {
+          setSize(size + 1);
+        }}
+      >
+        Load More
+      </button>
+    </div>
   );
 }
 
