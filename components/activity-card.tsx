6ded310a3c4ef1e1520b15f963e1f85e3f66e5f2
ibrahim <karroumiibrahim33@gmail.com>
2024-04-22 12:09:13 +0100

    Support the timeligne effect on the activitys

--- components/activity-card.tsx
+++ components/activity-card.tsx
@@ -1,6 +1,11 @@
 import { Avatar, Modal } from "@mantine/core";
 import moment from "moment";
 import { useState } from "react";
+import { FaRegEdit } from "react-icons/fa";
+import { IoIosAddCircleOutline, IoMdAddCircleOutline } from "react-icons/io";
+import { LuDelete } from "react-icons/lu";
+import { MdDeleteForever, MdOutlineEdit } from "react-icons/md";
+import { TbEdit } from "react-icons/tb";
 interface ActivityCardI {
   activity: any;
 }
@@ -11,7 +16,7 @@ interface ActivityCardI {
     console.log(dateObject.toLocaleString('en-US'))
     console.log({ activities }); */
 const ActivityCard = ({ activity }: ActivityCardI) => {
-//   const [avatar, setAvatar] = useState("kj");
+  //   const [avatar, setAvatar] = useState("kj");
   const date = Date.now();
   console.log(date);
   console.log({ date });
@@ -19,48 +24,44 @@ const ActivityCard = ({ activity }: ActivityCardI) => {
   const entity = activity.eventType.split("_")[0];
   console.log({ action });
   console.log({ entity });
-//   switch (action) {
-//     case "created":
-//       {
-//         setAvatar("create");
-//       }
-//       break;
-//     case "updated":
-//       {
-//         setAvatar("update");
-//       }
-//       break;
-//     case "deleted":
-//       {
-//         setAvatar("delete");
-//       }
-//       break;
-//     default: {
-//       setAvatar(" ");
-//     }
-//   }
+ 
+  const actionIcons = {
+    created: "<IoIosAddCircleOutline />", // Replace with your icon name or path
+    updated: "<MdOutlineEdit />", // Replace with your icon name or path
+    deleted: "<LuDelete />", // Replace with your icon name or path
+  };
 
   return (
     <>
-      <div className="m-5">
-        <div className="flex items-center gap-3">
-          <div className="">
-            <Avatar src={activity.user.image} size={"sm"} className="">
-              {activity.user.lastName[0]} {activity.user.firstName[0]}
-            </Avatar>{" "}
+      
+        <div className="flex items-center justify-center gap-3">
+          <div className="text-lg step text-slate-500">
+            
+            
+              {action === "created" && entity!=="post" && <IoMdAddCircleOutline />}
+              {action === "updated" && entity === 'post' && <TbEdit />}
+              {action === "updated" && entity!=='post' && <MdOutlineEdit />}
+              {action === "deleted" && <MdDeleteForever />}
+              {action === "created" && entity === "post" && (
+                <img src={activity.user.image} alt="" className="w-5 h-5 rounded-full" />
+              )}
+           
           </div>
-          <div className="flex gap-1">
+          <div className="flex gap-1 text-slate-900">
             <div className="text-xs font-semibold">
               {activity.user.lastName} {activity.user.firstName}
             </div>
-            <div className="text-xs">
-              {action} {entity} at{" "}
-              {moment(activity.submitTime).format("hh:mm:ss")}{" "}
-              {moment(activity.submitTime).format("MM/DD/YYYY")}
+            <div className="text-xs text-slate-600 ">
+              {action} {entity} on{" "}
+              {moment(activity.submitTime).format("DD MMMM YYYY")} at{" "}
+              <span className=" hover:text-slate-900">
+
+              {moment(activity.submitTime).format("hh:mm:ss")}
+              </span>
             </div>
           </div>
         </div>
-      </div>
+     
     </>
   );
 };
