c22c94acde2b2e69c43604ee9fc6fbfb987dfb12
ibrahim <karroumiibrahim33@gmail.com>
2024-04-29 22:16:29 +0100

    .

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,17 +2,45 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import fetcher from "../../lib/fetcher";
 import PostCard from "../../components/posts-card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/dialog";
 
 import { useEffect, useState } from "react";
 
 import Main from "../../layouts/main-layout";
 import AuthLayout from "../../layouts/auth-layout";
-import { Skeleton } from "@mantine/core";
+import { Avatar, Modal, Skeleton } from "@mantine/core";
 import { useInView } from "react-intersection-observer";
+import { useDisclosure } from "@mantine/hooks";
+import { GrLinkNext, GrLinkPrevious } from "react-icons/gr";
+import { IoMdArrowBack } from "react-icons/io";
+import EventListener from "@/components/event-listener";
+import { toast } from "react-toastify";
+import httpClientReq from "@/lib/http-client-req";
+import { CiImageOn } from "react-icons/ci";
 
 const PAGE_SIZE = 4;
 
+enum EventTypes {
+  PostCreated = "post_created",
+  PostUpdated = "post_updated",
+  CommentCreated = "comment_created",
+  CommentUpdated = "comment_updated",
+  CommentDeleted = "comment_deleted",
+}
 function Home() {
+  
+  
+
+
+
+
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1);
   const [isFetching, setIsFetching] = useState<boolean>(false);
@@ -55,32 +83,31 @@ function Home() {
   };
 
   return (
-    <section className="grid justify-center w-full text-black body-font">
-      <div className="p-4 bg-white max-sm:max-w-sm lg:max-w-screen-lg">
-        <div className="flex flex-col items-center gap-12">
-          {posts.data.map((post) => (
-            <PostCard
-              key={post._id}
-              post={post}
-              onClick={() => router.push(`/posts/${post._id}`)}
-              onUpdate={undefined}
-              
-            />
-          ))}
+    <>
+      <section className="grid justify-center w-full text-black body-font">
+        <div className="p-4 bg-white max-sm:max-w-sm lg:max-w-screen-lg">
+          <div className="flex flex-col items-center gap-12">
+            {posts.data.map((post) => (
+              <PostCard
+                key={post._id}
+                post={post}
+                onClick={() => router.push(`/posts/${post._id}`)}
+                onUpdate={undefined}
+              />
+            ))}
 
-          <div ref={loadMoreRef}></div>
-          <Skeleton
-            hidden={isEndOfList}
-            height={420}
-            width={480}
-            radius="md"
-            className="mb-2"
-          />
+            <div ref={loadMoreRef}></div>
+            <Skeleton
+              hidden={isEndOfList}
+              height={420}
+              width={480}
+              radius="md"
+              className="mb-2"
+            />
+          </div>
         </div>
-      </div>
-      {/* {isFetching && <Loader color="blue" />} */}
-      {/* {isEndOfList && <Text className="text-center">End of posts</Text>} */}
-    </section>
+      </section>
+    </>
   );
 }
 
